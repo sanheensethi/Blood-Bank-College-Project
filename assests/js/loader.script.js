7f9b80cdad956fb97c36dccbefe7e2a4e74327df
sanheensethi <sanheensethi37659@gmail.com>
2018-03-08 08:38:16 +0530

    Modified Loader Enabled

---
+++ assests/js/loader.script.js
@@ -0,0 +1,8 @@
+allcon = document.getElementById('allcon');
+allcon.style.display='none';
+window.onload = function(){
+					allcon = document.getElementById('allcon');
+					allcon.style.display='block';
+					centerloader = document.getElementById('loader');
+					centerloader.style.display='none';
+				}
